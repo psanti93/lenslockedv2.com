b6d35ce419ac2f1ef00e145bf08c49817e1c06a6
Paul Santiago <paulsantiago282@gmail.com>
2022-12-09 15:03:11 -0500

    Make example with Chi

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/psanti93/lenslockedv2.com
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.8 // indirect
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -61,7 +63,14 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
