e38edd3925b3787b9d69ecbbd38655acebb39bff
Paul Santiago <paulsantiago282@gmail.com>
2022-12-09 13:42:33 -0500

    Create handler type

--- main.go
+++ main.go
@@ -13,27 +13,22 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p> to get in touch email me at <a href=\"mailto:paulsantiago282@gmail.com\">paulsantiago282@gmail.com</a>")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Page Not Found</h1>")
-}
+// beneifit is that you'd get access to different fields that get defined in them
+type Router struct{}
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		notFoundHandler(w, r)
-		//OR
-		//http.Error(w, "Page Not Found", http.StatusNotFound)
+		http.Error(w, "Page Not Found", http.StatusNotFound)
 	}
-
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
