d60ff3c187e4af2001a25677bdbcea51e8c0b158
Paul Santiago <paulsantiago282@gmail.com>
2022-12-27 15:27:39 -0500

    Add postgres struct and function to derive env variables

--- cmd/exp/postgresExercise/main.go
+++ cmd/exp/postgresExercise/main.go
@@ -7,10 +7,33 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBname   string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBname, cfg.SSLMode)
+}
+
 func main() {
 
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "baloo",
+		Password: "junglebook",
+		DBname:   "lenslockedv2",
+		SSLMode:  "disable",
+	}
+
 	//pgx is the database driver
-	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslockedv2 sslmode=disable")
+	db, err := sql.Open("pgx", cfg.String())
 
 	if err != nil {
 		panic(err)
