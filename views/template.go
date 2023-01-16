6f6db6d88729c83b9b36350b9bec8fd881e6f84b
Paul Santiago <paulsantiago282@gmail.com>
2023-01-16 17:08:15 -0500

    Add notes of populating the csrfField function

--- views/template.go
+++ views/template.go
@@ -22,6 +22,7 @@ func ParseFs(fs fs.FS, patterns ...string) (Template, error) {
 
 	// We need to create the custom function prior to parsing the templates
 	tmpl := template.New(patterns[0])
+	// 1. create a placeholder function of the crsField first
 	tmpl = tmpl.Funcs(
 		template.FuncMap{
 			"csrfField": func() template.HTML {
@@ -46,8 +47,18 @@ type Template struct {
 }
 
 func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}) {
-	tmpl := t.htmlTmpl
-	//Example of a race condtion. Where multple objects will be using the same object with multple requests
+
+	// Solve race condition by creating a copy of template for each request using clone
+	tmpl, err := t.htmlTmpl.Clone()
+
+	if err != nil {
+		log.Printf("cloningTemplate: %v", err)
+		http.Error(w, "There was an error rendering the page", http.StatusInternalServerError)
+		return
+	}
+
+	// 2. when we execute the populate the placeholder that we define in 1
+
 	tmpl = tmpl.Funcs(
 		template.FuncMap{
 			"csrfField": func() template.HTML {
@@ -57,7 +68,7 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	if err := tmpl.Execute(w, data); err != nil {
+	if err = tmpl.Execute(w, data); err != nil {
 		log.Printf("Executing template:%v", err)
 		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
 		return
