0cd5f65deed53bb286e88a48c738ccf7bc1552be
Paul Santiago <paulsantiago282@gmail.com>
2023-01-16 09:31:50 -0500

    Add cookie

--- controllers/users.go
+++ controllers/users.go
@@ -74,5 +74,13 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	cookie := http.Cookie{
+		Name:  "email",
+		Value: user.Email,
+		Path:  "/",
+	}
+
+	http.SetCookie(w, &cookie)
+
 	fmt.Fprintf(w, "User authenticated: %+v", user)
 }
